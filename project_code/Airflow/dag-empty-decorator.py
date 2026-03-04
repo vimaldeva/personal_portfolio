17fe5584669c95efe7faa641b365885c6589b1ff
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-04 21:16:18 +0530

    Add empty DAG decorator for Airflow

---
+++ project_code/Airflow/dag-empty-decorator.py
@@ -0,0 +1,12 @@
+from airflow.decorators import dag
+from datetime import datetime
+
+
+@dag(start_date= datetime(2026,1,1),
+     schedule_interval= "@daily",
+     catchup= False)
+def dag_empty_decorator():
+    pass
+
+dag_empty_decorator()
+
