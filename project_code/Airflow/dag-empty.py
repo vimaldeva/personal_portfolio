b5075bdcb0c4b5ac0f6223db72f7823acdf96c2a
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-04 22:20:17 +0530

    Fix DAG parameters in dag-empty.py

--- project_code/Airflow/dag-empty.py
+++ project_code/Airflow/dag-empty.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 with DAG("dag-empty",
          start_date = datetime(2026,1,1),
-         schedule_interval= "@daily",
-         catchup= False
+         schedule= "@daily",
+         catchup = False
          ) as dag:
     pass
     
