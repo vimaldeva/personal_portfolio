8146fbdf8851d01573bb436791093ee31cb925cf
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-05 15:08:11 +0530

    Add Airflow DAG with greeting tasks

---
+++ project_code/Airflow/dag-scenario-branch-parallel.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from datetime import datetime
+# from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
+import logging
+from airflow.operators.bash import BashOperator
+
+
+def greet_task_a(**kwargs):
+    logging.info("Hello! This is task A")
+    # print("Hello! This is task A")
+
+def greet_task_b(**kwargs):
+    print("Hello! This is task B")
+
+def greet_task_c(**kwargs):
+    print("Hello! This is task C")
+
+def greet_task_d(**kwargs):
+    print("Hello! This is task D")
+
+with DAG("dag-dependency-python-branch",
+        start_date= datetime(2026,1,1),
+        schedule = "@daily",
+        catchup= False  ) as dag :    
+    task_1 = PythonOperator(task_id = "task_a",
+                            python_callable= greet_task_a)
+
+    task_2 = PythonOperator(task_id = "task_b",
+                            python_callable= greet_task_b)
+
+    task_3 = PythonOperator(task_id = "task_c",
+                            python_callable= greet_task_c)
+    
+    task_4 = PythonOperator(task_id = "task_d",
+                            python_callable= greet_task_d)
+    task_1 >> [task_2, task_3 ] >> task_4
