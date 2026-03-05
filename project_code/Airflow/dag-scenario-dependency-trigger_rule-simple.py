7b8939afc2f5289ffbf7509eb4f647630c3b3d04
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-05 15:52:00 +0530

    Create dag-scenario-dependency-trigger_rule-simple.py

---
+++ project_code/Airflow/dag-scenario-dependency-trigger_rule-simple.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from datetime import datetime
+# from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
+import logging
+from airflow.operators.bash import BashOperator
+
+
+def greet_task_a(**kwargs):
+    logging.info("Hello! This is task A")
+    # print("Hello! This is task A")
+
+def greet_task_b(**kwargs):
+    print("Hello! This is task B")
+
+with DAG("dag-dependency-trigger_rule",
+        start_date= datetime(2026,1,1),
+        schedule = "@daily",
+        catchup= False  ) as dag :    
+    task_1 = PythonOperator(task_id = "task_a",
+                            python_callable= greet_task_a)
+
+    task_2 = PythonOperator(task_id = "task_b",
+                            python_callable= greet_task_b,
+                            trigger_rule = "one_failed"
+                            )
+
+    task_1 >> task_2
