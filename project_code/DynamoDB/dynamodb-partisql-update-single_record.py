5945a06ac12bdddf79f6ade6e6c390fb501ca81a
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-07 21:41:33 +0530

    Create dynamodb-partisql-update-single_record.py

---
+++ project_code/DynamoDB/dynamodb-partisql-update-single_record.py
@@ -0,0 +1,13 @@
+import boto3
+from botocore.exceptions import ClientError
+from pprint import pprint
+
+dynamodb_client = boto3.client(
+            'dynamodb',
+            region_name='ap-south-1'   )
+
+statement_1 = "UPDATE ExamMarks SET marks = 80 WHERE roll_no = 102 AND subject_cd = 'MATH-101'"
+dynamodb_client.execute_statement(Statement = statement_1)
+
+# pprint(a.get("Items"))
+
