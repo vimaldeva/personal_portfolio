f9f045a8d66c6907147c5d51e4612b751d175cf1
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-07 20:56:56 +0530

    Update dynamodb-update-update_item.py

--- project_code/DynamoDB/dynamodb-update-update_item.py
+++ project_code/DynamoDB/dynamodb-update-update_item.py
@@ -12,3 +12,10 @@ response = client.update_item(
     },
     ReturnValues="UPDATED_NEW" # Returns only the updated attributes
 )
+
+
+update_item (Modify specific fields) :
+
+Unlike put_item (which replaces the whole item), update_item allows you to change just one attribute (e.g., update marks) while leaving the rest (e.g., student name) alone. It also supports atomic counters (incrementing values).
+
+Key Concept: You use an UpdateExpression string.
