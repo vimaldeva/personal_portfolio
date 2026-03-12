c0d0d2934990c3503e295d8df7fee7b560b52a7c
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-12 16:43:49 +0530

    Update databricks-endpoint-list.py

--- project_code/GenAI/Databricks/databricks-endpoint-list.py
+++ project_code/GenAI/Databricks/databricks-endpoint-list.py
@@ -1,1 +1,15 @@
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+os.environ["DATABRICKS_HOST"] = os.getenv("DATABRICKS_HOST") 
+os.environ["DATABRICKS_TOKEN"] = os.getenv("DATABRICKS_TOKEN")
+
+import databricks.sdk
+w = databricks.sdk.WorkspaceClient()
+endpoints = w.serving_endpoints.list()
+for ep in endpoints:
+    print(f"Available: {ep.name}")
