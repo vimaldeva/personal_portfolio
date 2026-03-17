10eb4a41e298961f9f97ed94c05f7399136c87cc
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-17 23:19:18 +0530

    Create sql_solution.sql

---
+++ core_skills/SQL/Interview-Handson/sql-cricket-points_table/sql_solution.sql
@@ -0,0 +1,24 @@
+WITH combined_data AS
+(
+	SELECT  team_1
+	       ,team_2
+	       ,winner
+	FROM icc_world_cup
+	UNION
+	SELECT  team_2
+	       ,team_1
+	       ,winner
+	FROM icc_world_cup
+) , winner_flag AS
+(
+	SELECT  team_1
+	       ,winner
+	       ,CASE WHEN team_1 = winner THEN 1  ELSE 0 END AS win_flag
+	FROM combined_data
+)
+SELECT  team_1
+       ,COUNT(team_1)                   AS matches_played
+       ,SUM(win_flag)                   AS no_of_wins
+       ,(COUNT(team_1) - SUM(win_flag)) AS no_of_losses
+FROM winner_flag
+GROUP BY  team_1
