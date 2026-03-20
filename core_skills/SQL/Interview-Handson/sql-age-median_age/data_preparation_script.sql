318fcb726c48de1cbce4bfc9d3715928240f173d
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-20 11:03:55 +0530

    Create data_preparation_script.sql

---
+++ core_skills/SQL/Interview-Handson/sql-age-median_age/data_preparation_script.sql
@@ -0,0 +1,20 @@
+create table emp(
+emp_id int,
+emp_name varchar(20),
+department_id int,
+salary int,
+manager_id int,
+emp_age int);
+
+insert into emp
+values
+(1, 'Ankit', 100,10000, 4, 39),
+(2, 'Mohit', 100, 15000, 5, 48),
+(3, 'Vikas', 100, 10000,4,37),
+(4, 'Rohit', 100, 5000, 2, 16),
+(5, 'Mudit', 200, 12000, 6,55),
+(6, 'Agam', 200, 12000,2, 14),
+(7, 'Sanjay', 200, 9000, 2,13),
+(8, 'Ashish', 200,5000,2,12),
+(9, 'Mukesh',300,6000,6,51),
+(10, 'Rakesh',300,7000,6,50);
