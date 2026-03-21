e6525a47da3e2d6afeaca49dc3b5ef9afcd12280
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-03-21 16:04:48 +0530

    Add files via upload

---
+++ core_skills/python/DSA/LinkedList/program/python-LL-pop.py
@@ -0,0 +1,70 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        if self.head == None :
+            print("Empty List")
+        else :
+            current_node = self.head
+            while current_node.next != None :
+                print(current_node.value)
+                current_node = current_node.next   
+            print(current_node.value)         
+
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else :
+            current_node = self.head
+            while current_node.next != None :
+                current_node = current_node.next
+                
+            self.tail = new_node
+            current_node.next = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.head == None :
+            return
+        else :
+            current_node = self.head
+
+            if current_node.next == None :
+                popped_value = current_node.value
+                self.head = None
+                self.tail = None
+                return popped_value
+
+            else :
+                while current_node.next.next != None :
+                    current_node = current_node.next
+
+                popped_value = current_node.next.value
+                current_node.next = None
+                self.tail = current_node
+                return popped_value
+        self.length -= 1
+
+a = LinkedList(2)
+a.append(5)
+a.append(15)
+a.append(20)
+a.append(35)
+a.append(50)
+
+a.print_list()
+print('testling pop')
+result = a.pop()
+print(result)
