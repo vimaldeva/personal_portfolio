6980028f1c73d1032e58134996371ec3ae7eaa80
vimaldeva <40683504+vimaldeva@users.noreply.github.com>
2026-04-01 08:35:19 +0530

    Update cli-connect_aws_cli.py

--- important commands/aws/cli-connect_aws_cli.py
+++ important commands/aws/cli-connect_aws_cli.py
@@ -6,3 +6,11 @@ aws s3 ls
 
 # create new AWS connection using access keys
 aws configure
+
+# Configure using SSO access keys
+$env:AWS_REGION = "us-east-1"
+$env:AWS_DEFAULT_REGION = "us-east-1"
+$env:AWS_ACCESS_KEY_ID="AAAAAA"
+$env:AWS_SECRET_ACCESS_KEY="/AAA/AAAA"
+$env:AWS_SESSION_TOKEN= "IAAAAA"
+
