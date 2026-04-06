0bfde3e5f7116dd27377c568b14b8a9d4fdc72e3
Vimal <vimaldeva10@gmail.com>
2026-04-06 21:14:31 +0530

    adding strands files

--- Knowledge_center/execution_samples/GenAI/strands/Agents/single_agent-with_tools.py
+++ Knowledge_center/execution_samples/GenAI/strands/Agents/single_agent-with_tools.py
@@ -1,12 +1,13 @@
 import boto3
 from strands import Agent, tool
 from strands.models import BedrockModel
+from strands_tools import http_request  # This provides list of prebuilt tools in strands from community
 
 
 dynamodb = boto3.client('dynamodb', region_name = 'ap-south-1')
 
 @tool
-def get_table_list():
+def get_table_list() -> list:
     """ This tool connects to AWS account and returns list of tables in DynamoDB """
     response = dynamodb.list_tables()
     return response.get('TableNames', [])
@@ -17,9 +18,9 @@ model = BedrockModel(
 )
 agent = Agent(
     model=model,
-    tools=[get_table_list],
+    tools=[get_table_list ],
     system_prompt="You're a helpful assistant. You can do simple math calculation, and tell the weather." )
 
-response = agent("Tell me list of tables available in dynamoDB")
+response = agent("Tell me list of dynammodb tables available")
 
 print(response)
